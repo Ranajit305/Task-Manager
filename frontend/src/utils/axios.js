5ca049db249cfb26913cdd40d8540e498048f565
Ranajit305 <nathranajit6@gmail.com>
2025-04-26 18:32:31 +0530

    axios update

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosUrl = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000/api',
+    baseURL: `${import.meta.env.VITE_BACKEND_URL}/api` || 'http://localhost:5000/api',
     withCredentials: true
 })
